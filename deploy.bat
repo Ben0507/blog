206bb09ee37c5a1742c7e7a8053f6bd1d5729024
苏文斌 <931787301@qq.com>
2023-03-23 15:05:56 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ git add -A
 git commit -m 'auto-deploy'
 git remote add origin https://github.com/Ben0507/blog.git
 git pull
-git push --force origin HEAD:master
+git push origin HEAD:master
 REM To delete the dist folder
 echo Auto-Deploy-Complete!
 pause
